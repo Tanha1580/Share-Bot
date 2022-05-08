a91bf3aeb77593d2dced359bcccac3b7790c2058
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-05-08 19:34:57 +0430

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"🔰 ربات آپلودر اختصاصی\n\n\n💻برنامه‌نویسی شده توسط <u>𝗔𝗹𝗶</u>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("❌ بستن", callback_data = "close")
                     ]
                 ]
             )
