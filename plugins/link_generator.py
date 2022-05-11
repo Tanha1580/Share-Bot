f55c9f8398e3e5af1e33590fb6467cc7c6e39222
Tanha1580 <94787480+Tanha1580@users.noreply.github.com>
2022-05-11 12:02:14 +0430

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -3,7 +3,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, STATIC_NUM
 from helper_func import encode, get_message_id
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
@@ -33,7 +33,7 @@ async def batch(client: Client, message: Message):
             continue
 
 
-    string = f"{f_msg_id * int(str(abs(client.db_channel.id))[3:])}-{s_msg_id * int(str(abs(client.db_channel.id))[3:])}"
+    string = f"{(f_msg_id * int(str(abs(client.db_channel.id))[3:])) + STATIC_NUM}-{(s_msg_id * int(str(abs(client.db_channel.id))[3:])) + STATIC_NUM}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("• اشتراک گذاری لینک", url=f'https://telegram.me/share/url?url={link}')]])
@@ -54,7 +54,7 @@ async def link_generator(client: Client, message: Message):
             await channel_message.reply("❌ خطا\n\nاین [ پست / لینک ] از کانال دیتابیس نیست یا حذف شده است.", quote = True)
             continue
 
-    base64_string = await encode(f"{msg_id * int(str(abs(client.db_channel.id))[3:])}")
+    base64_string = await encode(f"{(msg_id * int(str(abs(client.db_channel.id))[3:])) + STATIC_NUM}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("• اشتراک گذاری لینک", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"🔗 لینک ایجاد شده برای فایل شما\n\n─═════─✦─═════─\n{link}", quote=True, reply_markup=reply_markup, disable_web_page_preview = True)
