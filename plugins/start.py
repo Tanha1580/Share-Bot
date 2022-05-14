50e02944415f804eefd3224f6c17b86057665dbe
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-05-14 15:57:51 +0430

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -114,7 +114,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "• عضویت",
-                url = f"https://t.me/+52uN5k7qyoI5ZDEx")
+                url = client.invitelink)
         ]
     ]
     try:
