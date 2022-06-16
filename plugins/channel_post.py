78380d87b113a25f923ec572ce6d44f90af64a66
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-16 18:31:02 +0430

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -28,7 +28,7 @@ async def channel_post(client: Client, message: Message):
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("• اشتراک گذاری لینک", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(f"🔗 لینک ایجاد شده برای فایل شما\n\n─═════─✦─═════─\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"🔗 لینک ایجاد شده برای فایل شما\n\n─══════─✦─══════─\n{link}\n─══════─✦─══════─\n\n🆔️ @{client.username}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
