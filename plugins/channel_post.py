b5273332e6ae30e1ae60999eb18da83839132cfd
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-16 18:58:53 +0430

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,7 +9,7 @@ from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, X_NUM
 from helper_func import encode, subscribed
 
-@Bot.on_message(filters.private & subscribed & ~filters.photo & ~filters.text & ~filters.sticker & ~filters.poll & ~filters.game)
+@Bot.on_message(filters.private & subscribed & ~filters.text & ~filters.sticker & ~filters.poll & ~filters.game)
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("⏳ لطفا صبر کنید ...", quote = True)
     try:
