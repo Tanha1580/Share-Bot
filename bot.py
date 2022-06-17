a2fd4c012fc816a815c34798168d8c6254aa2ac3
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-17 10:13:34 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ class Bot(Client):
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL_2)).invite_link
                 if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_2, creates_join_request = Tru)
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_2, creates_join_request = True)
                     link = (await self.get_chat(FORCE_SUB_CHANNEL_2)).invite_link
                 self.invitelink2 = link
             except Exception as a:
