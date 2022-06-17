77b9fc89f0c6f7861e4e7fbedfd1b733fd9e0c0b
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-17 15:36:51 +0430

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,10 +1,10 @@
 #(©)CodeXBotz
-from config import FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4
+from config import FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4, FORCE_SUB_CHANNEL_REQUEST_1, FORCE_SUB_CHANNEL_REQUEST_2, FORCE_SUB_CHANNEL_REQUEST_3, FORCE_SUB_CHANNEL_REQUEST_4
 from pyrogram.types import InlineKeyboardButton
 
 
 def fsub_button(client, message):
-    if FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4:
+    if FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4 and not FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3 and not FORCE_SUB_CHANNEL_REQUEST_4:
         buttons = [
             [
                 InlineKeyboardButton(text = "• عضویت اول", url = client.invitelink1)
@@ -19,7 +19,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4:
+    if FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4 and not FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3 and not FORCE_SUB_CHANNEL_REQUEST_4:
         buttons = [
             [
                 InlineKeyboardButton(text = "• عضویت اول", url = client.invitelink1)
@@ -37,7 +37,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4:
+    if FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4 and not FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3 and not FORCE_SUB_CHANNEL_REQUEST_4:
         buttons = [
             [
                 InlineKeyboardButton(text = "• عضویت اول", url = client.invitelink1)
@@ -58,7 +58,7 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
-    if FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and FORCE_SUB_CHANNEL_4:
+    if FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and FORCE_SUB_CHANNEL_4 and not FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3 and not FORCE_SUB_CHANNEL_REQUEST_4:
         buttons = [
             [
                 InlineKeyboardButton(text = "• عضویت اول", url = client.invitelink1)
@@ -71,6 +71,39 @@ def fsub_button(client, message):
             ],
             [
                 InlineKeyboardButton(text = "• عضویت چهارم", url = client.invitelink4)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if not FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4 and FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3 and not FORCE_SUB_CHANNEL_REQUEST_4:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.reinvitelink1)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if not FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_4 and FORCE_SUB_CHANNEL_REQUEST_1 and FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3 and not FORCE_SUB_CHANNEL_REQUEST_4:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.reinvitelink1)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت دوم", url = client.reinvitelink2)
             ]
         ]
         try:
