c450ff4137fc2b5bcd1ed0cbbb26ba88c77ddb09
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-17 21:14:21 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ class Bot(Client):
             try:
                 link = (await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_1, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200)).invite_link
                 if not link:
-                    await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_1, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))
+                    await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_1, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200)
                     link = (await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_1, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))).invite_link
                 self.reinvitelink1 = link
             except Exception as a:
@@ -59,7 +59,7 @@ class Bot(Client):
             try:
                 link = (await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_2, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))).invite_link
                 if not link:
-                    await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_2, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))
+                    await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_2, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200)
                     link = (await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_2, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))).invite_link
                 self.reinvitelink2 = link
             except Exception as a:
@@ -87,7 +87,7 @@ class Bot(Client):
             try:
                 link = (await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_3, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))).invite_link
                 if not link:
-                    await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_3, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))
+                    await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_3, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200)
                     link = (await self.create_chat_invite_link(FORCE_SUB_CHANNEL_REQUEST_3, creates_join_request = True, expire_date = datetime.utcnow().datetime.utcnow()+43200))).invite_link
                 self.reinvitelink3 = link
             except Exception as a:
