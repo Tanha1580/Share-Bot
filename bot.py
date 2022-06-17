e4fa74272f5630a9cc39a5708a0d396af3221c8c
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-17 22:48:49 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,55 +28,55 @@ class Bot(Client):
         usr_bot_me = await self.get_me()
 
         if CUSTOM_LINK_1:
-           self.invitelink1 = CUSTOM_LINK_1
+            self.invitelink1 = CUSTOM_LINK_1
         else:
-        if FORCE_SUB_CHANNEL_1:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL_1)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_1)
+            if FORCE_SUB_CHANNEL_1:
+                try:
                     link = (await self.get_chat(FORCE_SUB_CHANNEL_1)).invite_link
-                self.invitelink1 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from First Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_1}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-                sys.exit()
+                    if not link:
+                        await self.export_chat_invite_link(FORCE_SUB_CHANNEL_1)
+                        link = (await self.get_chat(FORCE_SUB_CHANNEL_1)).invite_link
+                    self.invitelink1 = link
+                except Exception as a:
+                    self.LOGGER(__name__).warning(a)
+                    self.LOGGER(__name__).warning("Bot can't Export Invite link from First Force Sub Channel!")
+                    self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_1}")
+                    self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                    sys.exit()
 
         if CUSTOM_LINK_2:
-           self.invitelink2 = CUSTOM_LINK_2
+            self.invitelink2 = CUSTOM_LINK_2
         else:
-        if FORCE_SUB_CHANNEL_2:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL_2)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_2)
+            if FORCE_SUB_CHANNEL_2:
+                try:
                     link = (await self.get_chat(FORCE_SUB_CHANNEL_2)).invite_link
-                self.invitelink2 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Second Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_2}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-                sys.exit()
+                    if not link:
+                        await self.export_chat_invite_link(FORCE_SUB_CHANNEL_2)
+                        link = (await self.get_chat(FORCE_SUB_CHANNEL_2)).invite_link
+                    self.invitelink2 = link
+                except Exception as a:
+                    self.LOGGER(__name__).warning(a)
+                    self.LOGGER(__name__).warning("Bot can't Export Invite link from Second Force Sub Channel!")
+                    self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_2}")
+                    self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                    sys.exit()
 
         if CUSTOM_LINK_3:
-           self.invitelink3 = CUSTOM_LINK_3
+            self.invitelink3 = CUSTOM_LINK_3
         else:
-        if FORCE_SUB_CHANNEL_3:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL_3)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_3)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL_3)).invite_link
-                self.invitelink3 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Third Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-                sys.exit()
+             if FORCE_SUB_CHANNEL_3:
+             try:
+                 link = (await self.get_chat(FORCE_SUB_CHANNEL_3)).invite_link
+                 if not link:
+                     await self.export_chat_invite_link(FORCE_SUB_CHANNEL_3)
+                     link = (await self.get_chat(FORCE_SUB_CHANNEL_3)).invite_link
+                 self.invitelink3 = link
+             except Exception as a:
+                 self.LOGGER(__name__).warning(a)
+                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Third Force Sub Channel!")
+                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_3}")
+                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
