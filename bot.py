d1e1d86043ed2e271da48f55d129fc85a3810a5d
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-17 23:31:34 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 #(©)Codexbotz
 
-import asyncio
 import pyromod.listen
-from datetime import datetime
-from time import time
 from pyrogram import Client
 import sys
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_REQUEST_1, FORCE_SUB_CHANNEL_REQUEST_2, FORCE_SUB_CHANNEL_REQUEST_3, CUSTOM_LINK_1, CUSTOM_LINK_2, CUSTOM_LINK_3, CHANNEL_ID
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4, CUSTOM_LINK_1, CUSTOM_LINK_2, CUSTOM_LINK_3, CUSTOM_LINK_4, CHANNEL_ID
 
 class Bot(Client):
     def __init__(self):
@@ -77,6 +74,23 @@ class Bot(Client):
                      self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_3}")
                      self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
                      sys.exit()
+
+        if CUSTOM_LINK_4:
+            self.invitelink4 = CUSTOM_LINK_4
+        else:
+            if FORCE_SUB_CHANNEL_4:
+                try:
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL_4)).invite_link
+                    if not link:
+                        await self.export_chat_invite_link(FORCE_SUB_CHANNEL_4)
+                        link = (await self.get_chat(FORCE_SUB_CHANNEL_4)).invite_link
+                    self.invitelink4 = link
+                except Exception as a:
+                    self.LOGGER(__name__).warning(a)
+                    self.LOGGER(__name__).warning("Bot can't Export Invite link from First Force Sub Channel!")
+                    self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_1}")
+                    self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                    sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
