3c40a60a5afb617f349a1a90930d016306d6f1ce
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-17 18:33:21 +0430

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -130,6 +130,24 @@ def fsub_button(client, message):
         except IndexError:
             pass
         return buttons
+    if not FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.reinvitelink1)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت دوم", url = client.invitelink2)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
     if FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_REQUEST_1 and FORCE_SUB_CHANNEL_REQUEST_2 and FORCE_SUB_CHANNEL_REQUEST_3:
         buttons = [
             [
@@ -161,6 +179,90 @@ def fsub_button(client, message):
             ],
             [
                 InlineKeyboardButton(text = "• عضویت سوم", url = client.reinvitelink3)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if not FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and FORCE_SUB_CHANNEL_REQUEST_1 and FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.reinvitelink1)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت دوم", url = client.reinvitelink2)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت سوم", url = client.invitelink3)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if not FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and not FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and FORCE_SUB_CHANNEL_REQUEST_3:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.reinvitelink1)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت دوم", url = client.invitelink2)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت سوم", url = client.reinvitelink3)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FORCE_SUB_CHANNEL_1 and not FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and not FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.invitelink1)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت دوم", url = client.reinvitelink2)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت سوم", url = client.invitelink3)
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(text = "• عضو شدم | دانلود فایل", url = f"https://t.me/{client.username}?start={message.command[1]}")
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if not FORCE_SUB_CHANNEL_1 and FORCE_SUB_CHANNEL_2 and FORCE_SUB_CHANNEL_3 and FORCE_SUB_CHANNEL_REQUEST_1 and not FORCE_SUB_CHANNEL_REQUEST_2 and not FORCE_SUB_CHANNEL_REQUEST_3:
+        buttons = [
+            [
+                InlineKeyboardButton(text = "• عضویت اول", url = client.reinvitelink1)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت دوم", url = client.invitelink2)
+            ],
+            [
+                InlineKeyboardButton(text = "• عضویت سوم", url = client.invitelink3)
             ]
         ]
         try:
