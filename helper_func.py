bcbca25cc03fb87feab13581286f58f01bfb8ff1
AIi1513 <105161496+AIi1513@users.noreply.github.com>
2022-06-20 00:57:10 +0430

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -138,7 +138,6 @@ async def reconvert(text: str) -> str:
 
 async def encode(string):
     string_bytes = string.encode("ascii")
-    string_convert = strings.convert(string_bytes)
     base64_bytes = base64.urlsafe_b64encode(string_convert)
     base64_string = (base64_bytes.decode("ascii")).strip("=")
     return base64_string
@@ -146,8 +145,7 @@ async def encode(string):
 async def decode(base64_string):
     base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
-    string_reconvert = strings.reconvert(string_bytes) 
+    string_bytes = base64.urlsafe_b64decode(base64_bytes)
     string = string_reconvert.decode("ascii")
     return string
 
